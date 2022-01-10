7fa2f2e2330499b69beeb05c7bc5eedab0316b63
unknown <tassiolimar01@gmail.com>
2022-01-10 13:30:29 -0300

    2comit

--- wallet-criptomoeda/api/index.js
+++ wallet-criptomoeda/api/index.js
@@ -4,6 +4,8 @@ const routes = require('./routes')
 const app = express()
 const port = 3000
 
+//
+
 routes(app)
 
 app.listen(port, () => console.log(`CARTEIRA DE CRIPTOMOEDA ATIVA NA PORTA ${port}`))
